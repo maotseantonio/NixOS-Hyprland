bcf9d0c345e871dd0b897a74aeeeecfc3c3b00e2
installer <installer@gmail.com>
2024-12-11 19:08:53 +0630

    fornix

---
+++ home-manager/flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Home Manager configuration of antonio";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    wezterm.url = "github:wez/wezterm?dir=nix";
+    catppuccin.url = "github:catppuccin/nix";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+        url = "github:aylur/ags";
+        inputs.nixpkgs.follows = "nixpkgs";
+     };
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
+     nvchad4nix = {
+        url = "github:MOIS3Y/nvchad4nix";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvchad-on-steroids = {  # <- here
+      url = "github:maotseantonio/nvchad_config";
+      flake = false;
+    };
+    nixcord = {
+      url = "github:kaylorben/nixcord";
+    };
+    nyxexprs.url = "github:notashelf/nyxexprs";
+    yazi.url = "github:sxyazi/yazi";
+    textfox.url = "github:adriankarlen/textfox";
+   };
+  outputs = inputs @ { nixpkgs, home-manager, hyprpanel, catppuccin, hyprland, nixcord, textfox, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.hyprpanel.overlay
+          (final: prev: {
+        nvchad = inputs.nvchad4nix.packages."${pkgs.system}".nvchad;
+      })
+        ];
+      };
+
+    in {
+      homeConfigurations."antonio" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+        ./home.nix
+         #nixcord.homeManagerModules.nixcord
+          #ghostty.homeManagerModules.ghostty
+        textfox.homeManagerModules.default
+        #yazi.homeManagerModules.yazi
+        catppuccin.homeManagerModules.catppuccin
+        #{
+          #wayland.windowManager.hyprland = {
+          #  enable = true;
+          #  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          #};
+        #}
+        ];
+        extraSpecialArgs = { inherit inputs; };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
+
+
+
