a570df5a49991dc87c9f136272a37c0212dbb36c
maotseantonio <thetzinantonio@gmail.com>
2025-01-14 20:03:28 +0630

    HAHAHA

---
+++ modules/system/greetd.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  config,
+  host,
+  username,
+  options,
+  lib,
+  inputs,
+  system,
+  ...
+}:
+with lib; let
+  cfg = config.system.greetd;
+in {
+  options.system.greetd = {
+    enable = mkEnableOption "Enable Greetd Display Manager Services";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+         greetd.tuigreet
+    ];
+    services.displayManager.defaultSession = "hyprland";
+    services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+    programs.uwsm.enable = true;
+    programs.uwsm.waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = "Hyprland compositor manager by UWSM";
+        binPath = "/run/current-system/sw/bin/Hyprland";
+      };
+    };
+  };
+}
