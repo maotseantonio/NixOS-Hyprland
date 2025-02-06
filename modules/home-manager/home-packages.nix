d6cd3e5b09c3083bc3687ddbe361e2fa12beda55
Thet Zin Nyein <thetzinantonio@gmail.com>
2025-02-06 14:44:58 +0630

    Update home-packages.nix

--- modules/home-manager/home-packages.nix
+++ modules/home-manager/home-packages.nix
@@ -15,25 +15,25 @@
     hyprpicker
     #inputs.wezterm.packages.${pkgs.system}.default
     # inputs.zen-browser.packages."${pkgs.system}".default
-    inputs.nyxexprs.packages.${pkgs.system}.ani-cli
+ #   inputs.nyxexprs.packages.${pkgs.system}.ani-cli
     inputs.ags.packages.${pkgs.system}.agsFull
-    inputs.hyprsunset.packages.${pkgs.system}.hyprsunset
+   # inputs.hyprsunset.packages.${pkgs.system}.hyprsunset
     yazi
     # inputs.yazi.packages.${pkgs.system}.default
     microfetch
     gpu-screen-recorder
-    vscodium
+  #  vscodium
     libqalculate
-    libdbusmenu-gtk3
-    dbus-glib
-    gtkmm3
-    gtkmm4
-    gtkmm2
-    komikku
-    mangal
-    mangareader
-    lutgen
-    tmux
-    gtk4
+   # libdbusmenu-gtk3
+   # dbus-glib
+  #  gtkmm3
+ #   gtkmm4
+ #   gtkmm2
+ #   komikku
+#    mangal
+#    mangareader
+#    lutgen
+#    tmux
+#    gtk4
   ];
 }
